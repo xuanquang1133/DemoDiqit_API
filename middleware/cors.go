ffe2a855935dc00e4537e8144163dd1455e3bedc
vietbeesolutions <viet.pham@beesolutionsgroup.com>
2026-05-18 16:30:18 +0700

    Update multiple CORS

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,14 +2,22 @@ package middleware
 
 import (
 	"demodiqit_api/config"
+	"strings"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // CorsConfig returns the CORS middleware for Gin
 func CorsConfig(cfg *config.Config) gin.HandlerFunc {
+	var feOrigins []string
+	strs := strings.Split(cfg.FrontendOrigin, ";")
+	for _, feOrigin := range strs {
+		feOrigins = append(feOrigins, feOrigin)
+	}
+
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{cfg.FrontendOrigin},
+		AllowOrigins:     feOrigins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
